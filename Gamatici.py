fefe7ce7bb80661e7380c66fa780c1cfa7681058
Andreea Simona <92686562+Luz1210@users.noreply.github.com>
2023-11-30 13:37:01 +0200

    small changes and added some comments

--- Gamatici.py
+++ Gamatici.py
@@ -1,195 +1,158 @@
 import random
 
 class Grammar:
-    Vn: list
-    Vt: list
-    S: str
-    P: list
-    cuvinte_generate: set
-
     def __init__(self, Vn: list, Vt: list, P: list):
-        self.Vn = Vn
-        self.Vt = Vt
-        self.S = 'S'
-        self.P = P
-        self.cuvinte_generate = set()
-        
-    def inlocuire_lambda(self):
-        for i in range(len(self.P)):
-            temporary_tuple: tuple = self.P[i]
-            if temporary_tuple[1] == "lambda":
-                temporary_list = list(temporary_tuple)
-                temporary_list[1] = ""
-                self.P[i] = tuple(temporary_list)
-                
+        self.Vn: list = Vn
+        self.Vt: list = Vt
+        self.P: list = P
+        self.S: str = 'S'
+        self.generated_words: set = set()
 
-    def citire():
-        numar_elemente_Vn: int = int(input("Cate elemente are Vn? "))
-        Vn_temporar: list = [input(f"Introduceti elementul numarul {i} din Vn: ") for i in range(numar_elemente_Vn)]
-    
-        numar_elemente_Vt: int = int(input("Cate elemente are Vt? "))
-        Vt_temporar: list = [input(f"Introduceti elementul numarul {i} din Vt: ") for i in range(numar_elemente_Vt)]
-
-        numar_productii: int = int(input("Cate productii vor fi? "))
-        P_temporar = []
+    def citire(self):
+        self.Vn: list = []
+        self.Vt: list = []
+        self.P: list = []
+        numar_neterminale = int(input("Numarul de neterminale: "))
+        for i in range(numar_neterminale):
+            self.Vn.append(input("Neterminal: "))
+        numar_terminale = int(input("Numarul de terminale: "))
+        for i in range(numar_terminale):
+            self.Vt.append(input("Terminal: "))
+        numar_productii = int(input("Numarul de productii: "))
         for i in range(numar_productii):
             user_input = input("Introduceti pereche de tip cheie->valoare: ")
-            key,value = user_input.split("->")
-            P_temporar.append[(key, value)]
-            
-        for i in range(len(P_temporar)):
-            temporary_tuple: tuple = P_temporar[i]
-            if temporary_tuple[1] == "lambda":
-                temporary_list = list(temporary_tuple)
-                temporary_list[1] = ""
-                P_temporar[i] = tuple(temporary_list)
+            key, value = user_input.split("->")
+            self.P.append((key, value))
 
-        gramatica: Grammar = Grammar(Vn_temporar, Vt_temporar, P_temporar)
+        self.generated_words: set = set()
 
-        return gramatica
-
-    def citire_din_fisier(calea):
-        f = open(calea, "r")
-        Vn_string:str = f.readline()
-        Vn_string = Vn_string.replace("\n","")
-        Vn_temporar: list = Vn_string.split()
-        Vt_string:str = f.readline()
-        Vt_string = Vt_string.replace("\n","")
-        Vt_temporar: list = Vt_string.split()
+    def citire_fisier(self, file_name: str):
+        self.Vn: list = []
+        self.Vt: list = []
+        self.P: list = []
+        f = open(file_name, "r")
+        Vn_string: str = f.readline()
+        Vn_string = Vn_string.replace("\n", "")
+        self.Vn = Vn_string.split(" ")
+        Vt_string: str = f.readline()
+        Vt_string = Vt_string.replace("\n", "")
+        self.Vt = Vt_string.split(" ")
         numar_productii = int(f.readline())
-        P_temporar = []
         for i in range(numar_productii):
-            file_input:str = f.readline()
-            file_input = file_input.replace("\n","")
-            key,value = file_input.split("->")
-            P_temporar.append((key, value))
-            
-        for i in range(len(P_temporar)):
-            temporary_tuple: tuple = P_temporar[i]
-            if temporary_tuple[1] == "lambda":
-                temporary_list = list(temporary_tuple)
-                temporary_list[1] = ""
-                P_temporar[i] = tuple(temporary_list)
-                
-        gramatica: Grammar = Grammar(Vn_temporar, Vt_temporar, P_temporar)
-        return gramatica
-
+            file_input = f.readline()
+            file_input = file_input.replace("\n", "")
+            key, value = file_input.split("->")
+            self.P.append((key, value))
 
     def verificare(self):
-        i:str
+        i: str
         for neterminal in self.Vn:
+            #neterminal sa fie litera mare
             if neterminal.islower() == True:
                 return False
+            #neterminal sa nu fie si terminal
             if neterminal in self.Vt:
                 return False
-            
+
         for terminal in self.Vt:
+            #terminal sa nu fie si neterminal
             if terminal in self.Vn:
                 return False
-            
+
+        #simbolul de start sa fie neterminal
         if self.S not in self.Vn:
             return False
-        
+
         ok = False
         for productie in self.P:
             ok2 = True
+            #partea stanga a productiei sa aiba macar un neterminal
             for neterminal in self.Vn:
                 if neterminal in productie[0]:
                     ok2 = True
             if ok2 == False:
                 return False
+            #partea stanga a productiei sa fie neterminalul de start
             if self.S == productie[0]:
                 ok = True
         if ok == False:
             return False
-        
+
         for productie in self.P:
             for litera in productie[0]:
+                #fiecare litera din productie sa se afle in Vn sau Vt
                 if litera not in self.Vn and litera not in self.Vt:
                     return False
             for litera in productie[1]:
-                if litera not in self.Vn and litera not in self.Vt:
+                if litera not in self.Vn and litera not in self.Vt and productie[1] != 'lambda':
                     return False
-        
+
         return True
-    
+
     def is_regular(self):
         for productie in self.P:
             if len(productie[0]) != 1 or productie[0].isupper() == False:
                 return False
-            if len(productie[1])>2 or len(productie[1])<1:
+            if len(productie[1]) > 2 or len(productie[1]) < 1:
                 return False
-            
-            if len(productie[1])==1:
-                if productie[1].islower()==False:
+
+            if len(productie[1]) == 1:
+                if productie[1].islower() == False:
                     return False
-            if len(productie[1])==2:
+            if len(productie[1]) == 2:
                 if productie[1][0].islower == False or productie[1][1].isupper() == False:
                     return False
-                
+
         return True
-            
-        print()
-        
+
+
     def generare(self):
         cuvant: str = self.S
-        print(f"{cuvant} -> ", end = '')
+        print(f"{cuvant} -> ", end='')
         ok: bool = True
         while ok == True:
-            optiuni = []
+            optiuni_de_inlocuire = []
             optiune: tuple
             for optiune in self.P:
                 if optiune[0] in cuvant or optiune[0] == cuvant:
-                    optiuni.append(optiune)
-            inlocuire:tuple = random.choice(optiuni)
-            cuvant = cuvant.replace(inlocuire[0], inlocuire[1], 1)
-            print(cuvant, end = "")
+                    optiuni_de_inlocuire.append(optiune)
+            inlocuire_aleasa: tuple = random.choice(optiuni_de_inlocuire)
+            if inlocuire_aleasa[1] == "lambda":
+                cuvant = cuvant.replace(inlocuire_aleasa[0], '', 1)
+            else:
+                cuvant = cuvant.replace(inlocuire_aleasa[0], inlocuire_aleasa[1], 1)
+            print(cuvant, end="")
             ok = False
             for i in self.P:
                 if i[0] in cuvant:
                     ok = True
             if ok == True:
-                print(" -> ", end = "")
+                print(" -> ", end="")
             if ok == False:
-                self.cuvinte_generate.add(cuvant)
-                
-    def automat():
+                self.generated_words.add(cuvant)
         print()
-                
+
     def afisare(self):
-        print(f"multimea neterminalelor: {self.Vn}")
-        print(f"multimea terminalelor: {self.Vt}")
-        print(f"simbolul de start: {self.S}")
-        print("Productii:")
-        for i in self.P:
-            print(f"{i[0]} -> {i[1]}")
-            
+        print("Neterminale: ", self.Vn)
+        print("Terminale: ", self.Vt)
+        print("Productii: ", self.P)
+        print("Start: ", self.S)
+
 class FiniteAutomaton:
-    a:str
+    def __init__(self, Q: list, E: list, q0: str, F: list, delta: list):
+        self.Q: list = Q
+        self.E: list = E
+        self.q0: str = q0
+        self.F: list = F
+        self.delta: list = delta
+
+
 
 def main():
-    gramatica2: Grammar = Grammar(["S","B","C"],["a","b","c"],[("S", "aS"),
-                                                       ("S", "aA"),
-                                                       ("S", "aA"),
-                                                       ("A", "b"),
-                                                       ("A", "lambda")])
-    #gramatica2.inlocuire_lambda()
-    
-    gramatica: Grammar = Grammar.citire_din_fisier("C:\\OTHER\\temporar.txt")
+    grammar = Grammar([], [], [])
+    grammar.citire_fisier('C:\\Users\\User\\Documents\\scoala\\LFC\\gram\\gramatic\\gramatic\\gram.txt')
+    print(grammar.verificare())
+    grammar.generare()
+    grammar.afisare()
 
-    gramatica.afisare()
-    print(gramatica2.is_regular())
-    print (gramatica.verificare())
-    numar_cuvinte:int = int(input("Introduceti numarul de cuvinte dorite: "))
-    for i in range(numar_cuvinte):
-        print()
-        gramatica.generare()
-
-    print()    
-    #print(gramatica.cuvinte_generate)
-    print()
-    
 main()
-
-
-
